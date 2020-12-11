a29b032e7be07639a2e3005eca913c947c053637
Jordy <jordy_van_opstal@live.nl>
2020-12-11 13:03:31 +0100

    Apply Odin changes

--- Scripts/Node.cs
+++ Scripts/Node.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace XNode {
@@ -23,7 +24,7 @@ namespace XNode {
     /// </code>
     /// </example>
     [Serializable]
-    public abstract class Node : ScriptableObject {
+    public abstract class Node : SerializedScriptableObject {
         /// <summary> Used by <see cref="InputAttribute"/> and <see cref="OutputAttribute"/> to determine when to display the field value associated with a <see cref="NodePort"/> </summary>
         public enum ShowBackingValue {
             /// <summary> Never show the backing value </summary>
